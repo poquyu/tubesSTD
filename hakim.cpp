ebd33064e42566c098deefe3e95aa787ac84d2ce
Williamm <135003038+poquyu@users.noreply.github.com>
2023-12-11 00:33:43 +0700

    Update hakim.cpp

--- hakim.cpp
+++ hakim.cpp
@@ -13,11 +13,95 @@ adr_hakim alokasiHakim(dataHakim x) {
     adr_hakim P = new elemenHakim;
     info(P).nama = x.nama;
     info(P).NIP = x.NIP;
+    info(P).pendidikan = x.pendidikan;
     info(P).jabatan = x.jabatan;
     info(P).pangkat = x.pangkat;
-    info(P).alamat = x.alamat;
-    info(P).noTelp = x.noTelp;
+    info(P).usia = x.usia;
     nextHakim(P) = NULL;
     nextTerdakwa(P) = NULL;
     return P;
+}
+
+adr_terdakwa alokasiTerdakwa(dataTerdakwa x){
+    adr_terdakwa P = new elemenTerdakwa;
+    info(P).nama = x.nama;
+    info(P).alamat = x.alamat;
+    info(P).NIK = x.NIK;
+    info(P).pekerjaan = x.pekerjaan;
+    info(P).agama = x.agama;
+    info(P).jenisKelamin = x.jenisKelamin;
+    info(P).tempatTglLahir = x.tempatTglLahir;
+    nextTerdakwa(P) = NULL;
+    return P;
+}
+
+void insertFirstHakim(listHakim &L, adr_hakim P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+        nextHakim(P) = P; 
+    } else {
+        adr_hakim Q = first(L);
+        while (nextHakim(Q) != first(L)){
+            Q = nextHakim(Q);
+        }
+        nextHakim(P) = first(L);
+        first(L) = P;
+        nextHakim(Q) = P; 
+    }
+}
+
+void deleteHakim(listHakim &L, adr_hakim &P, string nip){
+    adr_hakim Q = first(L);
+    if (info(Q).NIP == nip) {
+        P = Q;
+        first(L) = nextHakim(Q);
+        nextHakim(last(L)) = first(L);
+        nextHakim(P) = NULL;
+    } else {
+        while (nextHakim(Q) != first(L) && info(nextHakim(Q)).NIP != nip) {
+            Q = nextHakim(Q);
+        }
+        if (info(nextHakim(Q)).NIP == nip) {
+            P = nextHakim(Q);
+            nextHakim(Q) = nextHakim(P);
+            nextHakim(P) = NULL;
+        }
+    }
+}
+
+void searchHakim(listHakim L, adr_hakim &P, string nip) {
+    adr_hakim Q = first(L);
+    while (nextHakim(Q) != first(L) && info(Q).NIP != nip) {
+        Q = nextHakim(Q);
+    }
+    if (info(Q).NIP == nip) {
+        P = Q;
+    }
+}
+
+void insertFirstTerdakwa(listTerdakwa &L, adr_terdakwa P){
+    if (first(L) == NULL) {
+        first(L) = P;
+    } else {
+        nextTerdakwa(P) = first(L);
+        first(L) = P;
+    }
+}
+
+void showListHakim(listHakim L) {
+    adr_hakim P = first(L);
+    do {
+        cout << "Nama: " << info(P).nama << endl;
+        cout << "NIP: " << info(P).NIP << endl;
+        cout << "Pendidikan: " << info(P).pendidikan << endl;
+        cout << "Jabatan: " << info(P).jabatan << endl;
+        cout << "Pangkat: " << info(P).pangkat << endl;
+        cout << "Usia: " << info(P).usia << endl;
+        cout << endl;
+        P = nextHakim(P);
+    } while (P != first(L));
+}
+
+void connectHakimTerdakwa(listHakim &LH, listTerdakwa &LT, adr_hakim &PH, adr_terdakwa &PT) {
+    nextTerdakwa(PH) = PT;
 }
